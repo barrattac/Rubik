5b9be3e2509c1c834a846c9f15f6ef39b02aeb31
Christopher Barratta <barrattac@hotmail.com>
2018-06-17 22:19:25 -0400

    Slow Rotations for Sides of Cube

---
+++ Assets/Scripts/UIBehavior/SideRotationGroup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class SideRotationGroup
+{
+    public GameObject CenterPiece { get; set; }
+    public Side SideToRotate { get; set; }
+    public SideRotateDirection DirectionToRotate { get; set; }
+    public float RotationSoFar { get; set; }
+
+    public SideRotationGroup(Side side, SideRotateDirection direction)
+    {
+        this.SideToRotate = side;
+        this.DirectionToRotate = direction;
+        this.RotationSoFar = 0;
+    }
+
+    internal bool HasStarted()
+    {
+        return this.RotationSoFar != 0;
+    }
+
+    internal bool HasFinished()
+    {
+        return Math.Abs(this.RotationSoFar) == 90 ;
+    }
+}

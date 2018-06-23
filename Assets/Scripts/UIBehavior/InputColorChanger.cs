7d4612b21ac4995b7dd5af72ea223c8a16280a35
Christopher Barratta <barrattac@hotmail.com>
2018-06-23 02:26:40 -0400

    Setup User Inputs for Solver Scene

---
+++ Assets/Scripts/UIBehavior/InputColorChanger.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InputColorChanger : MonoBehaviour {
+
+    public string SelectedColorAsString = "FFFFFF";
+    private Color SelectedColor = Color.white;
+    public string[] WhiteSideColors = new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF" };
+    public string[] YellowSideColors = new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFF00", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF" };
+    public string[] BlueSideColors = new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "0000FF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF" };
+    public string[] GreenSideColors = new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "00FF00", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF" };
+    public string[] OrangeSideColors = new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FEA100", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF" };
+    public string[] RedSideColors = new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FF0000", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF" };
+
+    public void OnColorSelected(string color)
+    {
+        this.SelectedColorAsString = color;
+        float r = (float)int.Parse(this.SelectedColorAsString.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+        float g = (float)int.Parse(this.SelectedColorAsString.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+        float b = (float)int.Parse(this.SelectedColorAsString.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+        this.SelectedColor = new Color(r/255, g/255, b/255);
+    }
+
+    public void OnPieceSelected(int piece)
+    {
+        Image clickedPiece = EventSystem.current.currentSelectedGameObject.GetComponent<Image>();
+        clickedPiece.color = this.SelectedColor;
+        int index = piece % 10;
+        switch (piece / 10)
+        {
+            case 0:
+                this.WhiteSideColors[index] = this.SelectedColorAsString;
+                break;
+            case 1:
+                this.RedSideColors[index] = this.SelectedColorAsString;
+                break;
+            case 2:
+                this.OrangeSideColors[index] = this.SelectedColorAsString;
+                break;
+            case 3:
+                this.BlueSideColors[index] = this.SelectedColorAsString;
+                break;
+            case 4:
+                this.GreenSideColors[index] = this.SelectedColorAsString;
+                break;
+            case 5:
+                this.YellowSideColors[index] = this.SelectedColorAsString;
+                break;
+        }
+    }
+
+    public void OnSolveButtonClicked()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UIBehavior/MainSceneScripts/MenuMainSceneController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuMainSceneController : MonoBehaviour {
+
+    private GameObject SceneMenu { get; set; }
+    public PovRotator PoV;
+    public SlowSideRotator Cube;
+    public AudioSource BGM;
+    public AudioSource SFX;
+
+    public Slider BGMSlider;
+    public Slider SFXSlider;
+    public Slider PoVRotateSlider;
+    public Slider CubeRotateSlider;
+
+    // Use this for initialization
+    void Start () {
+        this.SceneMenu = GameObject.FindGameObjectWithTag("MenuMainScene");
+        //this.BGM = GameObject.FindWithTag("MainCamera").GetComponent<AudioSource>();
+        //this.SFX = GameObject.FindWithTag("Cube").GetComponent<AudioSource>();
+        //this.PoV = GameObject.FindWithTag("PointOfView").GetComponent<PovRotator>();
+        //this.Cube = GameObject.FindWithTag("Cube").GetComponent<SlowSideRotator>();
+        this.SceneMenu.SetActive(false);
+    }
+
+
+
+    public void OnResumeButtonClick()
+    {
+        this.SceneMenu.SetActive(false);
+    }
+
+    public void OnBGMSliderChange()
+    {
+        this.BGM.volume = this.BGMSlider.value;
+    }
+
+    public void OnSFXSliderChange()
+    {
+        this.SFX.volume = this.SFXSlider.value;
+    }
+
+    public void OnPoVSpeedSliderChange()
+    {
+        this.PoV.rotationSpeed = this.PoVRotateSlider.value;
+    }
+
+    public void OnRotateSpeedSliderChange()
+    {
+        this.Cube.rotationSpeed = this.CubeRotateSlider.value;
+    }
+
+    public void OnMainMenuButtonClick()
+    {
+        OnResumeButtonClick();
+    }
+}
